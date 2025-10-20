69d9f1131e57f3751c75f23180008d9b393c3f9d
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2025-10-21 01:17:53 +0900

    シングルトン化

--- project/application/event/EventManager.cpp
+++ project/application/event/EventManager.cpp
@@ -1,1 +1,43 @@
 #include "EventManager.h"
+
+// シングルトン用インスタンス
+EventManager* EventManager::instance = nullptr;
+
+///====================================================
+/// シングルトンインスタンスの取得
+///====================================================
+EventManager* EventManager::GetInstance() {
+    // まだインスタンスが生成されていなければ作成
+    if (instance == nullptr) {
+        instance = new EventManager;
+    }
+    return instance;
+}
+
+///====================================================
+/// 終了処理
+///====================================================
+void EventManager::Finalize() {
+    // インスタンスを削除してnullptrに戻す
+    delete instance;
+	instance = nullptr;   
+}
+
+void  EventManager::Initialize() {
+
+}
+
+///====================================================
+/// 更新処理
+///====================================================
+void EventManager::Update() {
+
+}
+	
+void  EventManager::Drawo3Dbject(){
+
+}
+
+void  EventManager::Draw2DSprite(){
+
+}
--- project/application/event/EventManager.h
+++ project/application/event/EventManager.h
@@ -1,5 +1,48 @@
 #pragma once
-class EventManager
-{
+#include<Object3d.h>
+#include<Sprite.h>
+#include <vector>
+
+enum class EventState {
+	None,                // イベントなし
+	GameStart,           // ゲーム開始イベント
 };
 
+/// <summary>
+/// 各演出を管理するクラス
+/// </summary>
+class EventManager  {
+  private: // シングルトンインスタンス
+    static EventManager* instance;
+    // コピーコンストラクタを防ぐ
+    EventManager(EventManager&) = delete;
+    EventManager& operator=(EventManager&) = delete;
+public: // メンバ関数 
+    EventManager() = default;
+    ~EventManager() = default;
+    // シングルトンインスタンスの取得
+    static EventManager* GetInstance();
+    /// <summary>
+    /// 終了処理
+    /// </summary>
+    void Finalize();
+    /// <summary>
+    /// 初期化処理
+    /// </summary>
+	void Initialize();
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+	void Update();
+    /// <summary>
+    /// 描画処理
+    /// </summary>
+	void Drawo3Dbject();
+    void Draw2DSprite();
+
+
+    //void Start(EventState newState);
+private:
+    EventState state_ = EventState::None;
+    float timer_ = 0.0f;
+};
