d008348fb85bf0f9ab019127dd338dc0fe2ada66
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2025-10-22 22:41:54 +0900

    コメントアウトの追加

--- project/application/event/EventManager.h
+++ project/application/event/EventManager.h
@@ -4,6 +4,9 @@
 #include <vector>
 #include <memory>
 
+/// <summary>
+/// イベントの進行段階
+/// </summary>
 enum class EventPhase {
     Phase1,        // 第1段階（演出開始)
     Phase2,        // 第2段階
@@ -11,7 +14,9 @@ enum class EventPhase {
     Phase4,        // 第4段階
     End,           // 完了 (演出終了)
 };
-
+/// <summary>
+/// 発生するイベントの種類
+/// </summary>
 enum EventState {
     None,                // イベントなし
     GameStart,           // ゲーム開始イベント
@@ -46,44 +51,45 @@ public: // メンバ関数
     /// <summary>
     /// 描画処理
     /// </summary>
-	void Drawo3Dbject();
+    /// <summary>
+    /// 3Dオブジェクトの描画
+    /// </summary>
+    void Drawo3Dbject();
+    /// <summary>
+    /// 2Dスプライトの描画
+    /// </summary>
     void Draw2DSprite();
-       
-    //void Start(EventState newState);
-    
+    /// <summary>
+    /// ImGuiによるデバッグ描画
+    /// </summary>
     void DrawImGui();
-    
+
+    /// <summary>
+    /// イベント状態をリセット
+    /// </summary>
     void Reset();
-
 private:    // メンバ変数
-    // イベントの種類
-    EventState state_ = EventState::None;
-    float timer_ = 0.0f;
-    // イベントごとの演出フェーズ
-    EventPhase phase_ = EventPhase::Phase1;
-
-    std::unique_ptr <Object3d> object_ = nullptr;    
-    std::unique_ptr <Sprite> sprite_ = nullptr;
-    float alpha_;
-    
+    EventState state_ = EventState::None;      // イベントの種類
+    float timer_ = 0.0f;                       // 経過時間
+    EventPhase phase_ = EventPhase::Phase1;    // イベントごとの演出フェーズ
+    // 2Dスプライト
+    std::unique_ptr<Sprite> sprite_ = nullptr;
+    float alpha_ = 0.0f;                        // 不透明度 
+    // 上下から閉じる演出用スプライト  
     std::unique_ptr<Sprite> topSprite_ = nullptr;
     std::unique_ptr<Sprite> bottomSprite_ = nullptr;
-    Vector2 topPos_;
-    Vector2 bottomPos_; 
-    Vector2 size_;
-    float closeSpeed_ = 0.0f;
- 
+    Vector2 topPos_;                          // 上スプライトの座標
+    Vector2 bottomPos_;                       // 下スプライトの座標
+    Vector2 size_;                            // スプライトのサイズ
+    float closeSpeed_ = 0.0f;                 // スライド速度
+    // 文字演出スプライト
     std::unique_ptr<Sprite> mission_ = nullptr;
-    Vector2 missionsize_;
-    float missionalpha_;
-    float missionTimer_ = 0.0f;   // 経過時間を記録
-
-    bool timefige_;
-
-
-    bool isActive_ = false;    // イベント実行中フラグ
-    bool isFinished_ = false;  // イベント完了フラグ
-    
+    Vector2 missionsize_;                     // 文字スプライトサイズ
+    float missionalpha_ = 0.0f;               // 透明度
+    float missionTimer_ = 0.0f;               // 経過時間
+    bool timefige_ = false;                   // 時間制御フラグ？
+    bool isActive_ = false;                   // イベント実行中フラグ
+    bool isFinished_ = false;                 // イベント完了フラグ
 public:
     bool IsActive() const { return isActive_; }
     bool IsFinished() const { return isFinished_; }
