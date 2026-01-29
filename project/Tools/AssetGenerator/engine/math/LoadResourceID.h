662cc4b71e01bdaac22eb3a69d8dfee61fa5c352
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2026-01-29 14:53:33 +0900

    サブプロジェクトの結合完了

---
+++ project/Tools/AssetGenerator/engine/math/LoadResourceID.h
@@ -0,0 +1,25 @@
+#pragma once
+
+namespace LoadResourceID {
+
+    namespace texture {
+        constexpr const char* Arrow = "Textures/Ui/Arrow.png";
+        constexpr const char* Command01 = "Textures/Ui/command_01.png";
+        constexpr const char* Command02 = "Textures/Ui/command_02.png";
+        constexpr const char* Command11 = "Textures/Ui/command_11.png";
+        constexpr const char* Command12 = "Textures/Ui/command_12.png";
+        constexpr const char* Pausemenupanel = "Textures/Ui/PauseMenuPanel.png";
+        constexpr const char* Poseicon = "Textures/Ui/Poseicon.png";
+        constexpr const char* PoseiconRed = "Textures/Ui/Poseicon_red.png";
+        constexpr const char* Title = "Textures/Ui/Title.png";
+        constexpr const char* Ui02 = "Textures/Ui/UI_02.png";
+    }
+
+    namespace model {
+        constexpr const char* Player = "Models/Character/Player/Player.obj";
+    }
+
+    namespace audio {
+    }
+
+} // namespace LoadResourceID
