657882c5f89f3e5d61b77087bb3ae1486dc66a71
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2026-01-30 15:27:47 +0900

    不要なファルダの削除

--- project/Tools/AssetGenerator/engine/math/LoadResourceID.h
+++ project/Tools/AssetGenerator/engine/math/LoadResourceID.h
@@ -8,11 +8,14 @@ namespace LoadResourceID {
         constexpr const char* Command02 = "Textures/Ui/command_02.png";
         constexpr const char* Command11 = "Textures/Ui/command_11.png";
         constexpr const char* Command12 = "Textures/Ui/command_12.png";
+        constexpr const char* Complete = "Textures/Ui/Complete.png";
+        constexpr const char* Mission = "Textures/Ui/Mission.png";
         constexpr const char* Pausemenupanel = "Textures/Ui/PauseMenuPanel.png";
         constexpr const char* Poseicon = "Textures/Ui/Poseicon.png";
         constexpr const char* PoseiconRed = "Textures/Ui/Poseicon_red.png";
         constexpr const char* Target = "Textures/Ui/Target.png";
         constexpr const char* Title = "Textures/Ui/Title.png";
+        constexpr const char* Ui01 = "Textures/Ui/UI_01.png";
         constexpr const char* Ui02 = "Textures/Ui/UI_02.png";
     }
 
