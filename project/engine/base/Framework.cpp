b35acd7866f5ccfdabe8e3199012d3e816e5464c
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2026-03-30 07:24:12 +0900

    a

--- project/engine/base/Framework.cpp
+++ project/engine/base/Framework.cpp
@@ -77,7 +77,8 @@ void Framework::Initialize() {
     // WindowsAPIの初期化
     winApp = std::make_unique <WinApp>();
     // ウィンドウのタイトルを変更
-    winApp->SetWindowTitle(L"LE3B_07_オノデラ_ユヅキ_Skyraider");
+//    winApp->SetWindowTitle(L"LE4B_08_オノデラ_ユヅキ_Skyraider");
+    winApp->SetWindowTitle(L"Skyraider");
     winApp->Initialize();
     // DirectXの初期化
     dxCommon = std::make_unique <DirectXCommon>();
