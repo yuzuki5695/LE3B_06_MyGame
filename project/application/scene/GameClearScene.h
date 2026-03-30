9da377dcbc836f53432eb7a24d194dd0370e6eb9
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2026-03-30 16:55:05 +0900

    全シーンコードリセット

--- project/application/scene/GameClearScene.h
+++ project/application/scene/GameClearScene.h
@@ -1,12 +1,5 @@
 #pragma once
-#include<BaseScene.h>
-#include<Sprite.h>
-#include<Object3d.h>
-#include<ParticleEmitter.h>
-#include<SoundPlayer.h>
-#include <Skybox.h>
-#include<GameClearparticle.h>
-#include<Player.h>
+#include <BaseScene.h>
 
 /// <summary>
 /// ゲームクリアシーン
@@ -29,66 +22,6 @@ public: // メンバ関数
     /// 描画処理
     /// </summary>
     void Draw() override;
-
-
-    // クリア演出を段階的に行う
-    void UpdateStep();
-    // 各演出段階
-    void Step1_MovePlayerAndSwitchCamera();
-    void Step2_WaitOrDoSomething();
-    void Step3_MoveCameraOnInput();
-
-    bool EaseMove(Vector3& current, const Vector3& start, const Vector3& end, float& t, float speed);
-
 private: // メンバ変数
-    std::unique_ptr <Sprite> ui1_ = nullptr;     // スプライト 
-    std::unique_ptr <Skybox> Box_ = nullptr;     //　背景
-    
-    std::unique_ptr <Player> player_; 
-    Transform offset_;
 
-
-
-    uint32_t step_ = 0;
-
-    float easeT_ = 0.0f;  // 0.0 → 1.0 まで進めていく
-    float easeSpeed_ = 0.01f; // イージング速度
-    Vector3 startOffset_;
-    Vector3 endOffset_;
-    // Step1 カメラ
-    float step1CamT_ = 0.0f;
-    bool step1CamMoveStart_ = false;
-
-
-
-
-    float step2CamT_ = 0.0f;   // カメラX移動用のt
-    float step2Time_ = 0.0f;   // 上下揺れ用の時間
-    // GameClearScene.h にフラグ追加
-    bool step2FinishPlayerEase_ = false;
-    // GameClearScene.h
-    float step3Timer_ = 0.0f;           // Step3開始からの経過時間
-    bool step3FadeTriggered_ = false;   // 1秒後の短いフェード処理フラグ
-    // Step3制御
-    bool step3BackJumpDone_ = false;   // 後ろに下がる演出が完了したか
-    float startStep3PosX_ = 0.0f;     // Step3開始時のX
-
-
-    float startStep3PosZ_;
-
-    std::unique_ptr <Sprite> ui2_ = nullptr;     // スプライト      
-    std::unique_ptr <Sprite> ui3_ = nullptr;     // スプライト 
-
-    // UI移動制御用の変数
-    float ui2Timer_;
-    float ui2Duration_; // 約1秒
-    float ui3Timer_;
-    float ui3Duration_;
-
-    Vector2 ui2StartPos; // 上にずらした初期位置
-    Vector2 ui2EndPos; // 最終位置（現在の値）
-    Vector2 ui3StartPos; // 上にずらした初期位置
-    Vector2 ui3EndPos; // 最終位置（現在の値）
-    float step3TimeScale = 0.7f; // 0.3～0.6 あたりがおすすめ
-    std::unique_ptr <GameClearparticle> particle_;
 };
--- project/application/scene/GamePlayScene.cpp
+++ project/application/scene/GamePlayScene.cpp
@@ -28,11 +28,11 @@ using namespace MatrixVector;
 using namespace Easing;
 
 void GamePlayScene::Finalize() {
-    BulletManager::GetInstance()->Finalize();  // 弾の解放処理
-    FadeManager::GetInstance()->Finalize();    //  フェードマネージャの解放処理
-    EventManager::GetInstance()->Finalize();   //  イベントマネージャの解放処理
-    StageManager::GetInstance()->Finalize();   // ステージマネージャの解放処理
-	UIManager::GetInstance()->Finalize();      // UIマネージャの解放処理
+ //   BulletManager::GetInstance()->Finalize();  // 弾の解放処理
+ //   FadeManager::GetInstance()->Finalize();    //  フェードマネージャの解放処理
+ //   EventManager::GetInstance()->Finalize();   //  イベントマネージャの解放処理
+ //   StageManager::GetInstance()->Finalize();   // ステージマネージャの解放処理
+	//UIManager::GetInstance()->Finalize();      // UIマネージャの解放処理
 }
 
 void GamePlayScene::Initialize() {
@@ -40,76 +40,76 @@ void GamePlayScene::Initialize() {
     CameraManager::GetInstance()->Initialize(CameraTransform({ 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f }));
     CameraManager::GetInstance()->SetTypeview(ViewCameraType::Main);
 
-    // .objファイルからモデルを読み込む
-    ModelManager::GetInstance()->LoadModel(model::Goal);
+ //   // .objファイルからモデルを読み込む
+ //   ModelManager::GetInstance()->LoadModel(model::Goal);
 
-    // プレイヤーの作成と初期化
-    player_ = std::make_unique<Player>();
-    player_->Initialize(); // プレイヤーの初期化
-    CameraManager::GetInstance()->SetGamecameraTarget(player_->GetPlayerObject());
-    // カメラにプレイヤーを追わせる
+ //   // プレイヤーの作成と初期化
+ //   player_ = std::make_unique<Player>();
+ //   player_->Initialize(); // プレイヤーの初期化
+ //   CameraManager::GetInstance()->SetGamecameraTarget(player_->GetPlayerObject());
+ //   // カメラにプレイヤーを追わせる
 
-    // パーティクル
-    particles_ = std::make_unique<GamePlayparticle>();
-    particles_->Initialize(player_->GetPlayerObject());
+ //   // パーティクル
+ //   particles_ = std::make_unique<GamePlayparticle>();
+ //   particles_->Initialize(player_->GetPlayerObject());
 
-    enemy_ = std::make_unique<Enemy>();
-    enemy_->Initialize();
-    enemy_->SetPlayer(player_.get());
+ //   enemy_ = std::make_unique<Enemy>();
+ //   enemy_->Initialize();
+ //   enemy_->SetPlayer(player_.get());
 
-    // クリアゲート(仮)
-    wall = Object3d::Create(model::Goal, Transform{ { 2.0f, 2.0f, 2.0f }, { 0.0f, 0.0f, 0.0f }, { 8.0f, 39.0f, 800.0f } });
-    // スカイボックスの作成
-    TextureManager::GetInstance()->LoadTexture(texture::Cubemapbox);
-    Box_ = Skybox::Create(texture::Cubemapbox, Transform{ { 1000.0f, 1000.0f, 1000.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 100.0f } });
-    // ゴールフラグ初期化
-    goal_ = false;
-    // フェードマネージャの初期化
-    FadeManager::GetInstance()->Initialize();
-    // イベントマネージャの初期化
-    EventManager::GetInstance()->Initialize("gamestart");
-    // ステージマネージャの初期化
-    StageManager::GetInstance()->Initialize();
+ //   // クリアゲート(仮)
+ //   wall = Object3d::Create(model::Goal, Transform{ { 2.0f, 2.0f, 2.0f }, { 0.0f, 0.0f, 0.0f }, { 8.0f, 39.0f, 800.0f } });
+ //   // スカイボックスの作成
+ //   TextureManager::GetInstance()->LoadTexture(texture::Cubemapbox);
+ //   Box_ = Skybox::Create(texture::Cubemapbox, Transform{ { 1000.0f, 1000.0f, 1000.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 100.0f } });
+ //   // ゴールフラグ初期化
+ //   goal_ = false;
+ //   // フェードマネージャの初期化
+ //   FadeManager::GetInstance()->Initialize();
+ //   // イベントマネージャの初期化
+ //   EventManager::GetInstance()->Initialize("gamestart");
+ //   // ステージマネージャの初期化
+ //   StageManager::GetInstance()->Initialize();
 
-    end = false;
-    // ポーズメニューの初期化
-    pausemenu_ = std::make_unique<Pausemenu>();
-    pausemenu_->Initialize();
-    isPausedevent_ = false;
+ //   end = false;
+ //   // ポーズメニューの初期化
+ //   pausemenu_ = std::make_unique<Pausemenu>();
+ //   pausemenu_->Initialize();
+ //   isPausedevent_ = false;
 
-    // UIマネージャの初期化
-	UIManager::GetInstance()->Initialize();
+ //   // UIマネージャの初期化
+	//UIManager::GetInstance()->Initialize();
 }
-void GamePlayScene::PauseMenuUpdate() {
-    //  Enterキーでポーズの「開始」のみをチェック
-    if (isPausedevent_ && !pausemenu_->IsActive() && Input::GetInstance()->Triggrkey(DIK_TAB)) {
-        pausemenu_->SetActive(true); // 演出開始！
-    }
-    pausemenu_->Update();
-}
+//void GamePlayScene::PauseMenuUpdate() {
+//    //  Enterキーでポーズの「開始」のみをチェック
+//  //  if (isPausedevent_ && !pausemenu_->IsActive() && Input::GetInstance()->Triggrkey(DIK_TAB)) {
+//    //    pausemenu_->SetActive(true); // 演出開始！
+//    //}
+// //   pausemenu_->Update();
+//}
 
 void GamePlayScene::Update() {       
     // ---------------------------
     // ポーズメニュー処理
     // ---------------------------
 	// ポーズメニューの更新
-    PauseMenuUpdate();
-    if (pausemenu_->IsActive()) {
-        HandlePauseMenuCommands();  // ポーズ中のコマンド処理
-        return;                     // ポーズ中はその他の処理をスキップ
-    }
+   // PauseMenuUpdate();
+  ///  if (pausemenu_->IsActive()) {
+   //     HandlePauseMenuCommands();  // ポーズ中のコマンド処理
+    //    return;                     // ポーズ中はその他の処理をスキップ
+    //}
     // ---------------------------
     // フェード処理の開始
     // ---------------------------
-    HandleFadeInOut();
+   // HandleFadeInOut();
     // ---------------------------
     // イベントマネージャ更新
     // ---------------------------
-    EventManager::GetInstance()->Update(); 	
+   // EventManager::GetInstance()->Update(); 	
     // ---------------------------
     // プレイヤーの状態に応じた処理
     // ---------------------------
-    HandlePlayerState();       
+ //   HandlePlayerState();       
     // ---------------------------
     // ステージ更新
     // ---------------------------
@@ -118,52 +118,52 @@ void GamePlayScene::Update() {
     // カメラの更新処理
     // ---------------------------
     // サブカメラはプレイヤーを追わせる
-    CameraManager::GetInstance()->SetGamecameraTarget(player_->GetPlayerObject());
+ //  CameraManager::GetInstance()->SetGamecameraTarget(player_->GetPlayerObject());
     CameraManager::GetInstance()->Update();
 #pragma region 全てのObject3d個々の更新処理
-    // 終了しない限り更新処理
-    if (!end) {
-        // 更新処理
-        if (player_) {
-            player_->Update();
-        }
-        if (enemy_) {
-            enemy_->Update();
-        }
+    //// 終了しない限り更新処理
+    //if (!end) {
+    //    // 更新処理
+    //    if (player_) {
+    //        player_->Update();
+    //    }
+    //    if (enemy_) {
+    //        enemy_->Update();
+    //    }
 
-        // Bulletマネージャの更新処理
-        BulletManager::GetInstance()->Update();
+    //    // Bulletマネージャの更新処理
+    //    BulletManager::GetInstance()->Update();
 
-        // 当たり判定を常に実行
-        CollisionManager::GetInstance()->CheckAllCollisions();
-
-
-        wall->Update();
-
-        // 4. 【重要】削除処理は必ず「全ての更新・判定」が終わった最後に行う
-        if (enemy_ && enemy_->IsDead()) {
-            enemy_.reset(); // ここで初めて nullptr になる
-        }
-    }
-
-    // フェードアウトが完了したら次のシーンへ
-    if (FadeManager::GetInstance()->IsFadeEnd() && FadeManager::GetInstance()->GetFadeType() == FadeType::FadeOut) {
-        if (player_->GetState() == State::Dead) {
-            // シーン切り替え
-            SceneManager::GetInstance()->ChangeScene("GAMEOVER");
-        } //else if (player_->GetState() == State::Goal) {
-        //    // シーン切り替え
-        //    SceneManager::GetInstance()->ChangeScene("GAMECLEAR");
-        //}
-    }
-
-    // スカイボックス更新
-    Box_->Update(); 
+    //    // 当たり判定を常に実行
+    //    CollisionManager::GetInstance()->CheckAllCollisions();
 
 
-    // パーティクル更新
-    ParticleManager::GetInstance()->Update();
-    particles_->Update();
+    //    wall->Update();
+
+    //    // 4. 【重要】削除処理は必ず「全ての更新・判定」が終わった最後に行う
+    //    if (enemy_ && enemy_->IsDead()) {
+    //        enemy_.reset(); // ここで初めて nullptr になる
+    //    }
+    //}
+
+    //// フェードアウトが完了したら次のシーンへ
+    //if (FadeManager::GetInstance()->IsFadeEnd() && FadeManager::GetInstance()->GetFadeType() == FadeType::FadeOut) {
+    //    if (player_->GetState() == State::Dead) {
+    //        // シーン切り替え
+    //        SceneManager::GetInstance()->ChangeScene("GAMEOVER");
+    //    } //else if (player_->GetState() == State::Goal) {
+    //    //    // シーン切り替え
+    //    //    SceneManager::GetInstance()->ChangeScene("GAMECLEAR");
+    //    //}
+    //}
+
+    //// スカイボックス更新
+    //Box_->Update(); 
+
+
+    //// パーティクル更新
+    //ParticleManager::GetInstance()->Update();
+    //particles_->Update();
 #pragma endregion 全てのObject3d個々の更新処理
 
 #pragma region 全てのSprite個々の更新処理
@@ -194,181 +194,108 @@ void GamePlayScene::Draw() {
 #pragma region 全てのObject3d個々の描画処理
     // 箱オブジェクトの描画準備。3Dオブジェクトの描画に共通のグラフィックスコマンドを積む
     SkyboxCommon::GetInstance()->Commondrawing();
-    Box_->Draw();
+ //   Box_->Draw();
     // 3Dオブジェクトの描画準備。3Dオブジェクトの描画に共通のグラフィックスコマンドを積む
     Object3dCommon::GetInstance()->Commondrawing();
     StageManager::GetInstance()->Draw();
 
-    // 描画処理
-    if (!end) {
-        player_->Draw();
-        // Bulletマネージャの描画処理
-        BulletManager::GetInstance()->Draw();
-    }
+    //// 描画処理
+    //if (!end) {
+    //    player_->Draw();
+    //    // Bulletマネージャの描画処理
+    //    BulletManager::GetInstance()->Draw();
+    //}
 
-    if (enemy_) {
-        enemy_->Draw();
-    }
+    //if (enemy_) {
+    //    enemy_->Draw();
+    //}
 
-    wall->Draw();
-    // イベントマネージャの描画処理
-    EventManager::GetInstance()->Drawo3Dbject();
+    //wall->Draw();
+    //// イベントマネージャの描画処理
+    //EventManager::GetInstance()->Drawo3Dbject();
 
     // パーティクルの描画準備。パーティクルの描画に共通のグラフィックスコマンドを積む 
     ParticleCommon::GetInstance()->Commondrawing();
-    ParticleManager::GetInstance()->Draw();
+    //ParticleManager::GetInstance()->Draw();
 #pragma endregion 全てのObject3d個々の描画処理
 
 #pragma region 全てのSprite個々の描画処理
     // Spriteの描画準備。Spriteの描画に共通のグラフィックスコマンドを積む
     SpriteCommon::GetInstance()->Commondrawing();
-    // プレイヤーのUIスプライト描画
-    if (!end) {
-        player_->DrawSprite();
-    }
+    //// プレイヤーのUIスプライト描画
+    //if (!end) {
+    //    player_->DrawSprite();
+    //}
 
-    pausemenu_->Draw();
+    //pausemenu_->Draw();
   
 
     if (CameraManager::GetInstance()->GetTypeview() == ViewCameraType::Main) {
         UIManager::GetInstance()->Draw();
-    }
+    //}
 
-    // イベントマネージャの描画処理
-    EventManager::GetInstance()->Draw2DSprite();
-    // フェードマネージャの描画
-    FadeManager::GetInstance()->Draw();
+    //// イベントマネージャの描画処理
+    //EventManager::GetInstance()->Draw2DSprite();
+    //// フェードマネージャの描画
+    //FadeManager::GetInstance()->Draw();
 #pragma endregion 全てのSprite個々の描画処理
 }
-
-void GamePlayScene::HandlePauseMenuCommands() {
-    if (pausemenu_->GetCommand() == PauseCommand::GoToTitle) {
-        FadeManager::GetInstance()->Update();
-        if (!goal_) {
-            FadeManager::GetInstance()->StartFadeOut(1.0f, FadeStyle::Normal);
-            goal_ = true;
-        }
-        if (FadeManager::GetInstance()->IsFadeEnd()) {
-            SceneManager::GetInstance()->ChangeScene("TITLE");
-        }
-    }
-}
-
-void GamePlayScene::HandleFadeInOut() {
-    if (!FadeManager::GetInstance()->IsFadeStart() && !FadeManager::GetInstance()->IsFading()) {
-        FadeManager::GetInstance()->StartFadeIn(1.0f, FadeStyle::SilhouetteExplode);
-    }
-    FadeManager::GetInstance()->Update();
-}
-
-void GamePlayScene::HandlePlayerState() {
-    // 死亡演出
-    if (player_->GetState() == State::Dead && CameraManager::GetInstance()->GetTypeview() == ViewCameraType::Main) {
-        CameraManager::GetInstance()->SetMode(CameraMode::Default);
-        CameraManager::GetInstance()->SetTypeview(ViewCameraType::Sub);
-        end = false;
-    }
-
-    // サブカメラ時の処理
-    if (CameraManager::GetInstance()->GetTypeview() == ViewCameraType::Sub && CameraManager::GetInstance()->GetMode() == CameraMode::Default) {
-        FadeManager::GetInstance()->StartFadeOut(1.0f, FadeStyle::Normal);
-        CameraManager::GetInstance()->SetMode(CameraMode::Follow);
-    }
-
-    // ゴール判定
-    if (!goal_ && player_->GetState() == State::Alive && player_->GetPosition().z >= CameraManager::GetInstance()->GetGameplayCamera()->GetBezierPoints().back().controlPoint.z) {
-        FadeManager::GetInstance()->StartFadeOut(1.0f, FadeStyle::Normal);
-       // player_->SetState(State::Goal);
-        goal_ = true;
-        end = false;
-    }
-
-    // ゲーム開始後にプレイヤー操作可能に
-    if (player_->GetState() == State::None && EventManager::GetInstance()->IsFinished()) {
-        player_->SetState(State::Alive);
-        isPausedevent_ = true;
-        if (auto gpUI = UIManager::GetInstance()->GetUI<GamePlayUI>()) {
-            gpUI->StartStageProgressUI();
-        }
-    }
-
-    // 操作UIのアニメーション開始
-    if (EventManager::GetInstance()->GetProgress() >= 2.5f) {
-        if (auto gpUI = UIManager::GetInstance()->GetUI<GamePlayUI>()) {
-            gpUI->StartControlUIAnimation();
-        }
-    }
-}
-
-
-/////====================================================
-///// プレイヤー弾 vs 敵の当たり判定
-/////====================================================
-//void GamePlayScene::CheckBulletEnemyCollisionsOBB() {
-//    const auto& bullets = BulletManager::GetInstance()->GetPlayerBullets();
 //
-//    for (const std::unique_ptr<PlayerBullet>& bullet : bullets) {
-//        if (!bullet->IsActive()) continue;
-//
-//        for (auto& enemy : enemies_) {
-//            if (!enemy->IsActive()) continue;
-//
-//            OBB bulletOBB = bullet->GetOBB(); // bulletがOBB情報を持っている必要あり
-//            OBB enemyOBB = enemy->GetOBB();   // 敵のOBBも同様に
-//
-//            if (IsOBBIntersect(bulletOBB, enemyOBB)) {
-//                bullet->SetInactive();
-//                enemy->OnHit();
-//                
-//               // particles_->AddHitPosition(enemy->GetPosition());
-//                // パーティクル生成など
-//                break;
-//            }
+//void GamePlayScene::HandlePauseMenuCommands() {
+//    if (pausemenu_->GetCommand() == PauseCommand::GoToTitle) {
+//        FadeManager::GetInstance()->Update();
+//        if (!goal_) {
+//            FadeManager::GetInstance()->StartFadeOut(1.0f, FadeStyle::Normal);
+//            goal_ = true;
+//        }
+//        if (FadeManager::GetInstance()->IsFadeEnd()) {
+//            SceneManager::GetInstance()->ChangeScene("TITLE");
 //        }
 //    }
 //}
-/////====================================================
-///// 敵弾 vs プレイヤーの当たり判定
-/////====================================================
-//void GamePlayScene::CheckEnemyBulletPlayerCollisionsOBB() {
-//    const auto& bullets = BulletManager::GetInstance()->GetEnemyBullets();
 //
-//    OBB playerOBB = player_->GetOBB(); // プレイヤーがOBBを返すようにしておく必要あり
+//void GamePlayScene::HandleFadeInOut() {
+//    if (!FadeManager::GetInstance()->IsFadeStart() && !FadeManager::GetInstance()->IsFading()) {
+//        FadeManager::GetInstance()->StartFadeIn(1.0f, FadeStyle::SilhouetteExplode);
+//    }
+//    FadeManager::GetInstance()->Update();
+//}
 //
-//    for (const std::unique_ptr<EnemyBullet>& bullet : bullets) {
-//        if (!bullet->IsActive()) continue;
+//void GamePlayScene::HandlePlayerState() {
+//    // 死亡演出
+//    if (player_->GetState() == State::Dead && CameraManager::GetInstance()->GetTypeview() == ViewCameraType::Main) {
+//        CameraManager::GetInstance()->SetMode(CameraMode::Default);
+//        CameraManager::GetInstance()->SetTypeview(ViewCameraType::Sub);
+//        end = false;
+//    }
 //
-//        OBB bulletOBB = bullet->GetOBB(); // 弾にもOBBが必要
+//    // サブカメラ時の処理
+//    if (CameraManager::GetInstance()->GetTypeview() == ViewCameraType::Sub && CameraManager::GetInstance()->GetMode() == CameraMode::Default) {
+//        FadeManager::GetInstance()->StartFadeOut(1.0f, FadeStyle::Normal);
+//        CameraManager::GetInstance()->SetMode(CameraMode::Follow);
+//    }
 //
-//        if (IsOBBIntersect(bulletOBB, playerOBB)) {
-//            bullet->SetInactive();
-//            player_->SetState(State::Dead);
+//    // ゴール判定
+//    if (!goal_ && player_->GetState() == State::Alive && player_->GetPosition().z >= CameraManager::GetInstance()->GetGameplayCamera()->GetBezierPoints().back().controlPoint.z) {
+//        FadeManager::GetInstance()->StartFadeOut(1.0f, FadeStyle::Normal);
+//        // player_->SetState(State::Goal);
+//        goal_ = true;
+//        end = false;
+//    }
 //
-//            end = true;
-//            // ヒットエフェクトなど追加
-//            break;
+//    // ゲーム開始後にプレイヤー操作可能に
+//    if (player_->GetState() == State::None && EventManager::GetInstance()->IsFinished()) {
+//        player_->SetState(State::Alive);
+//        isPausedevent_ = true;
+//        if (auto gpUI = UIManager::GetInstance()->GetUI<GamePlayUI>()) {
+//            gpUI->StartStageProgressUI();
 //        }
 //    }
-//}
-/////====================================================
-///// 敵 vs プレイヤー の当たり判定
-/////====================================================
-//void GamePlayScene::CheckEnemyPlayerCollisionsOBB() {
-//    // プレイヤーのOBBを取得
-//    OBB playerOBB = player_->GetOBB();
 //
-//    for (auto& enemy : enemies_) {
-//        if (!enemy->IsActive() || enemy->IsDead()) continue;
-//
-//        // 敵のOBBを取得
-//        OBB enemyOBB = enemy->GetOBB();
-//
-//        // 衝突判定
-//        if (IsOBBIntersect(playerOBB, enemyOBB)) {
-//            player_->SetState(State::Dead);
-//            end = true; // ゲームオーバーへ遷移など
-//
-//            break;
+//    // 操作UIのアニメーション開始
+//    if (EventManager::GetInstance()->GetProgress() >= 2.5f) {
+//        if (auto gpUI = UIManager::GetInstance()->GetUI<GamePlayUI>()) {
+//            gpUI->StartControlUIAnimation();
 //        }
 //    }
 //}
