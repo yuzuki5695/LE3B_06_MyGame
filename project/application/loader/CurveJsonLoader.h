ef8ea5f2eb77cd970d2445f766dc35803cab6295
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2026-04-05 22:31:55 +0900

    レールカメラ処理の復元

---
+++ project/application/loader/CurveJsonLoader.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <string>
+#include <Vector3.h>
+#include <vector>
+
+namespace MyGame {
+    /// <summary>
+    /// ベジェ曲線の1点分の制御情報を保持する構造体
+    /// </summary>
+    struct BezierPoint {
+        MyEngine::Vector3 handleLeft;
+        MyEngine::Vector3 controlPoint;
+        MyEngine::Vector3 handleRight;
+        bool passed = true;  // ← この制御点を通過したか
+    };
+
+    /// <summary>
+    /// JSONファイルからベジェ曲線制御点を読み込むクラス
+    /// </summary>
+    class CurveJsonLoader {
+    public:
+        /// <summary>
+        /// 指定したパスのJSONファイルを読み込み、ベジェ制御点リストを返す
+        /// </summary>
+        /// <param name="filePath">JSONファイルのパス</param>
+        /// <returns>BezierPointの配列</returns>s
+        std::vector<BezierPoint> LoadBezierFromJSON(const std::string& filePath);
+    };
+}
