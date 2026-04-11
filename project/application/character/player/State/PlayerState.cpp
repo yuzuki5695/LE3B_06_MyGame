4e139bf0128e836cbb5367d846b80b30991b71e4
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2026-04-12 01:50:49 +0900

    namespace囲い忘れ

--- project/application/character/player/State/PlayerState.cpp
+++ project/application/character/player/State/PlayerState.cpp
@@ -5,51 +5,52 @@
 #include <CameraManager.h>
 
 using namespace MyEngine;
-using namespace MyGame;
 
-void PlayerStateIdle::Update(BaseCharacter& character) {
-    //if (Input::GetInstance()->Triggrkey(DIK_RETURN)) {
+namespace MyGame {
+    void PlayerStateIdle::Update(BaseCharacter& character) {
+        //if (Input::GetInstance()->Triggrkey(DIK_RETURN)) {
         character.ChangeState(std::make_unique<PlayerStateMove>());
-    //}
-    CameraManager::GetInstance()->SetTarget(character.GetObject3d());
-}
-
-void PlayerStateMove::Update(BaseCharacter& character) {
-    CameraManager::GetInstance()->SetTarget(character.GetObject3d());
-    
-    // BaseCharacterをPlayerにキャスト
-    Player* player = static_cast<Player*>(&character);
-
-    // 必要なコンポーネント
-    PlayerMove* move = player->GetMove();
-    PlayerReticle* reticle = player->GetReticle();
-    PlayerAttack* attack = player->GetAttack();
-    Object3d* object = player->GetObject3d();
-    Sprite* sprite = player->GetSprite();     
-    Object3d* target = player->GetTarget();
-
-    if (move && object) {
-
-        // 2. 移動処理の実行
-        move->Update(object->GetTransform(), 0.17f);
-
-        // ① 入力でレティクル移動
-        reticle->UpdateInput();
-
-        // ② スプライト位置更新
-        sprite->SetPosition(reticle->GetScreenPos());
-
-        // ③ ワールド座標に変換
-        Vector3 worldPos = reticle->ScreenToWorld(reticle->GetScreenPos(), CameraManager::GetInstance()->GetActiveCamera());
-
-        player->SetAimWorldPos(worldPos);
-        target->SetTranslate(player->GetAimWorldPos());
-        
-        attack->Update(object->GetTransform(), player->GetAimWorldPos());
-        
+        //}
+        CameraManager::GetInstance()->SetTarget(character.GetObject3d());
     }
-}
 
-void PlayerStateDead::Update(BaseCharacter& character) {
-	character.Destroy();
+    void PlayerStateMove::Update(BaseCharacter& character) {
+        CameraManager::GetInstance()->SetTarget(character.GetObject3d());
+
+        // BaseCharacterをPlayerにキャスト
+        Player* player = static_cast<Player*>(&character);
+
+        // 必要なコンポーネント
+        PlayerMove* move = player->GetMove();
+        PlayerReticle* reticle = player->GetReticle();
+        PlayerAttack* attack = player->GetAttack();
+        Object3d* object = player->GetObject3d();
+        Sprite* sprite = player->GetSprite();
+        Object3d* target = player->GetTarget();
+
+        if (move && object) {
+
+            // 2. 移動処理の実行
+            move->Update(object->GetTransform(), 0.17f);
+
+            // ① 入力でレティクル移動
+            reticle->UpdateInput();
+
+            // ② スプライト位置更新
+            sprite->SetPosition(reticle->GetScreenPos());
+
+            // ③ ワールド座標に変換
+            Vector3 worldPos = reticle->ScreenToWorld(reticle->GetScreenPos(), CameraManager::GetInstance()->GetActiveCamera());
+
+            player->SetAimWorldPos(worldPos);
+            target->SetTranslate(player->GetAimWorldPos());
+
+            attack->Update(object->GetTransform(), player->GetAimWorldPos());
+
+        }
+    }
+
+    void PlayerStateDead::Update(BaseCharacter& character) {
+        character.Destroy();
+    }
 }
