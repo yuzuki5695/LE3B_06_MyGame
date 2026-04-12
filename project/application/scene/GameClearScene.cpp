bcfae6ee229464a58adf80e8bb7f2a8481855d25
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2026-04-12 23:56:42 +0900

    シーンクラスに基本動作配置

--- project/application/scene/GameClearScene.cpp
+++ project/application/scene/GameClearScene.cpp
@@ -1,22 +1,53 @@
 #include "GameClearScene.h"
+#include <CameraManager.h>
+#include <SkyboxCommon.h>
+#include <SpriteCommon.h>
+#include <Object3dCommon.h>
+#include <ParticleCommon.h>
 
 using namespace MyEngine;
 
-
 namespace MyGame {
 	void GameClearScene::Finalize() {
-
+		CameraManager::GetInstance()->Finalize(); // カメラマネージャの終了処理
 	}
 
-	void GameClearScene::Initialize() {
+	void GameClearScene::Initialize() {		
+		CameraManager::GetInstance()->Initialize(Transform({ 1.0f, 1.0f, 1.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, -50.0f }));
 
 	}
 
 	void GameClearScene::Update() {
+#pragma region 全てのObject3d個々の更新処理			
+		CameraManager::GetInstance()->Update();
 
+
+#pragma endregion 全てのObject3d個々の更新処理
+
+#pragma region 全てのSprite個々の更新処理
+
+
+#pragma endregion 全てのSprite個々の更新処理
 	}
 
 	void GameClearScene::Draw() {
+#pragma region 全てのObject3d個々の描画処理
+		// 箱オブジェクトの描画準備。3Dオブジェクトの描画に共通のグラフィックスコマンドを積む
+		SkyboxCommon::GetInstance()->Commondrawing();
 
+		// 3Dオブジェクトの描画準備。3Dオブジェクトの描画に共通のグラフィックスコマンドを積む
+		Object3dCommon::GetInstance()->Commondrawing();
+
+
+		// パーティクルの描画準備。パーティクルの描画に共通のグラフィックスコマンドを積む 
+		ParticleCommon::GetInstance()->Commondrawing();
+
+#pragma endregion 全てのObject3d個々の描画処理
+
+#pragma region 全てのSprite個々の描画処理 
+		// Spriteの描画準備。Spriteの描画に共通のグラフィックスコマンドを積む
+		SpriteCommon::GetInstance()->Commondrawing();
+
+#pragma endregion 全てのSprite個々の描画処理
 	}
 }
--- project/application/scene/GameOverScene.cpp
+++ project/application/scene/GameOverScene.cpp
@@ -1,19 +1,51 @@
 #include "GameOverScene.h"
+#include <CameraManager.h>
+#include <SkyboxCommon.h>
+#include <SpriteCommon.h>
+#include <Object3dCommon.h>
+#include <ParticleCommon.h>
 
 using namespace MyEngine;
 
 namespace MyGame {
 	void GameOverScene::Finalize() {
-
+		CameraManager::GetInstance()->Finalize(); // カメラマネージャの終了処理
 	}
 
-	void GameOverScene::Initialize() {
+	void GameOverScene::Initialize() {	
+		CameraManager::GetInstance()->Initialize(Transform({ 1.0f, 1.0f, 1.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, -50.0f }));
 
 	}
 
 	void GameOverScene::Update() {
+#pragma region 全てのObject3d個々の更新処理			
+		CameraManager::GetInstance()->Update();
+
+
+#pragma endregion 全てのObject3d個々の更新処理
+
+#pragma region 全てのSprite個々の更新処理
+
+#pragma endregion 全てのSprite個々の更新処理
 	}
 
 	void GameOverScene::Draw() {
+	#pragma region 全てのObject3d個々の描画処理
+		// 箱オブジェクトの描画準備。3Dオブジェクトの描画に共通のグラフィックスコマンドを積む
+		SkyboxCommon::GetInstance()->Commondrawing();
+
+		// 3Dオブジェクトの描画準備。3Dオブジェクトの描画に共通のグラフィックスコマンドを積む
+		Object3dCommon::GetInstance()->Commondrawing();
+
+
+		// パーティクルの描画準備。パーティクルの描画に共通のグラフィックスコマンドを積む 
+		ParticleCommon::GetInstance()->Commondrawing();
+#pragma endregion 全てのObject3d個々の描画処理
+
+#pragma region 全てのSprite個々の描画処理 
+		// Spriteの描画準備。Spriteの描画に共通のグラフィックスコマンドを積む
+		SpriteCommon::GetInstance()->Commondrawing();
+
+#pragma endregion 全てのSprite個々の描画処理
 	}
 }
