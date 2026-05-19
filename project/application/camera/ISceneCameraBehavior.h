497a2f036b27a2799232820979a8625c56d2c11e
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2026-05-19 19:26:31 +0900

    関数の整理

--- project/application/camera/ISceneCameraBehavior.h
+++ project/application/camera/ISceneCameraBehavior.h
@@ -5,38 +5,34 @@
 #include <CameraSet.h>
 
 namespace MyGame {
-
     /// <summary>
     /// 各シーンカメラ挙動インターフェース
     /// </summary>
     class ISceneCameraBehavior {
-    public:
+    public: // メンバ関数
         virtual ~ISceneCameraBehavior() = default;
-
         /// <summary>
-        /// 挙動の初期化（シーン切り替え時に一度呼ばれる）
+        /// 初期化処理
         /// </summary>
         /// <param name="camera">操作対象のカメラポインタ</param>
         virtual void Initialize(MyEngine::Camera* camera) = 0;
-
         /// <summary>
-        /// 挙動の毎フレーム更新
+        /// 毎フレーム更新
         /// </summary>
         /// <param name="camera">操作対象のカメラポインタ</param>
         virtual void Update(MyEngine::Camera* camera) = 0;
-
-        // 対象のオブジェクトをセットする関数
+        /// <summary>
+        /// 操作対象のオブジェクトをセットする関数
+        /// </summary>
+        /// <param name="target"></param>
         virtual void SetTargetObject(MyEngine::Object3d* target) { target_ = target; }
-
-        // 進捗率(0.0~1.0)を返す仮想関数。デフォルトは0を返す
-        virtual float GetProgress() const { return 0.0f; }
-    protected:
-		// 対象のオブジェクト
+    protected: // メンバ変数
+        // 対象のオブジェクト
         MyEngine::Object3d* target_{};
         // カメラの状態データ
         MyEngine::CameraDefs::StateData stateData_;
     public:
-		// getter
+        // getter
         const MyEngine::CameraDefs::StateData& GetStateData() const { return stateData_; }
         // setter
         void SetCameraState(const MyEngine::CameraDefs::CameraState& state) { stateData_.state = state; }
