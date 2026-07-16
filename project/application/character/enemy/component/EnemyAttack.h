c1cf653962348fa87b94aff735a9fdde575c9684
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2026-07-17 05:01:54 +0900

    変数及び関数等の名称方法の統一

--- project/application/character/enemy/component/EnemyAttack.h
+++ project/application/character/enemy/component/EnemyAttack.h
@@ -4,13 +4,14 @@
 #include <EnemyType.h>
 
 namespace MyGame {
-
+    /// <summary>
+    /// 敵の攻撃状態
+    /// </summary>
     enum class AttackState {
-        Dash,
-        Charge, // 溜め
-        Idle
+        Dash,   // 突撃中
+        Charge, // 突撃前の溜め状態
+        Idle    // 待機状態
     };
-
     /// <summary>
     /// 敵の攻撃クラス
     /// </summary>
@@ -26,32 +27,37 @@ namespace MyGame {
         /// <param name="transform"></param>
         /// <param name="aimWorldPos"></param>
         void Update(EnemyType type, MyEngine::Object3d* object, const MyEngine::Vector3& aimWorldPos);
-
+        /// <summary>
+        /// 射撃タイプの攻撃更新
+        /// </summary>
+        /// <param name="object">敵オブジェクト</param>
+        /// <param name="aimWorldPos">狙うワールド座標</param>
         void UpdateShot(MyEngine::Object3d* object, const MyEngine::Vector3& aimWorldPos);
-
+        /// <summary>
+        /// 突撃タイプの攻撃更新
+        /// </summary>
+        /// <param name="object">敵オブジェクト</param>
+        /// <param name="aimWorldPos">狙うワールド座標</param>
         void UpdateTackle(MyEngine::Object3d* object, const MyEngine::Vector3& aimWorldPos);
     private: // メンバ変数
-        float timer_ = 0.0f;
-        float coolTime_ = 5.0f;   // 発射間隔
-        bool canShoot_; // 発射可能フラグ
-        float speed;
+        float timer_;      // 攻撃タイマー
+        float coolTime_;   // 発射間隔
+        bool canShoot_;    // 発射可能フラグ
+        float speed;       // 弾の移動速度
         // 乱数生成器
-        std::random_device rd;// 乱数生成器
-        std::mt19937 randomEngine_;
-        std::uniform_real_distribution<float> distcooltime_;
-
+        std::random_device rd;                                  // 乱数生成器
+        std::mt19937 randomEngine_;                             // 乱数生成器
+        std::uniform_real_distribution<float> distcooltime_;    // クールタイム決定用乱数
         // 状態遷移
         AttackState state_;
-
-
         // 突撃用変数
-        MyEngine::Vector3 dashVelocity_;
-        float dashSpeed_ = 1.5f;
-        float dashDistance_ = 0.0f;
-        float maxDashDistance_ = 25.0f;
-        float dashRotationSpeed_ = 1.5f;
-        float chargeTimer_ = 0.0f;
-        float chargeDuration_ = 1.5f; // 溜め時間
-        MyEngine::Vector3 chargeStartPos_;
+        MyEngine::Vector3 dashVelocity_;    // 突撃速度
+        float dashSpeed_;                   // 突撃速度
+        float dashDistance_;                // 現在の突撃距離
+        float maxDashDistance_;             // 最大突撃距離
+        float dashRotationSpeed_;           // 突撃中の回転速度
+        float chargeTimer_;                 // 溜め時間タイマー
+        float chargeDuration_;              // 溜め時間
+        MyEngine::Vector3 chargeStartPos_;  // 溜め開始位置
     };
 }
